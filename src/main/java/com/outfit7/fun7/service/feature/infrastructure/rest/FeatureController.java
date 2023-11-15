339000bfe578da04e747d0e4aa2607282899da79
anton_soroshkin <Anton_Soroshkin@epam.com>
2023-11-15 17:07:40 +0100

    Add some logging

--- src/main/java/com/outfit7/fun7/service/feature/infrastructure/rest/FeatureController.java
+++ src/main/java/com/outfit7/fun7/service/feature/infrastructure/rest/FeatureController.java
@@ -2,6 +2,8 @@ package com.outfit7.fun7.service.feature.infrastructure.rest;
 
 import com.outfit7.fun7.service.feature.api.ServiceCheckOperations;
 import com.outfit7.fun7.service.feature.api.dto.UserFeatures;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ import javax.validation.constraints.NotBlank;
 @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 class FeatureController {
 
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
   private final ServiceCheckOperations serviceCheckOperations;
 
   FeatureController(ServiceCheckOperations serviceCheckOperations) {
@@ -25,6 +29,7 @@ class FeatureController {
   UserFeatures getUserFeatureState(@RequestParam(required = false) String timezone,
                                    @RequestParam @NotBlank String userId,
                                    @RequestParam(name = "cc") @NotBlank String countryCode) {
+    logger.info("Retrieving feature states for user with id = {}", userId);
     return serviceCheckOperations.getUserFeatures(timezone, userId, countryCode);
   }
 }
